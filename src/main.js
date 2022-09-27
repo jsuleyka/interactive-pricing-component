286d5562470c84ee07dd6e549855cfad1427df6c
jsuleyka <suleymjb15@hotmail.com>
2022-09-27 15:50:37 -0600

    desktop design

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 let slider = document.getElementById("myRange");
-let output = document.getElementById("price");
+let output = document.getElementById("price-xs");
+let output2 = document.getElementById("price-lg");
 let outputViews = document.getElementById("views");
 output.innerHTML = '$' + parseFloat(slider.value).toFixed(2);
+output2.innerHTML = '$' + parseFloat(slider.value).toFixed(2);
 
 slider.oninput = function() {
   output.innerHTML = '$' + parseFloat(this.value).toFixed(2);
+  output2.innerHTML = '$' + parseFloat(this.value).toFixed(2);
   // outputViews.innerHTML = 
 }
