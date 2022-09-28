31d70232c4f186e2b0ff6e234e8b2614e6665eda
jsuleyka <suleymjb15@hotmail.com>
2022-09-28 11:12:33 -0600

    add price with descount

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
-let slider = document.getElementById("myRange");
+let slider = document.getElementById("myRange"), valSlider = slider.value ;
 let output = document.getElementById("price-xs");
 let output2 = document.getElementById("price-lg");
 let outputViews = document.getElementById("views");
-output.innerHTML = '$' + parseFloat(slider.value).toFixed(2);
-output2.innerHTML = '$' + parseFloat(slider.value).toFixed(2);
+let inputCheck = document.getElementById("check"), isChecked = false, discount = 0;
+
+output.innerHTML = '$' + parseFloat(valSlider).toFixed(2);
+output2.innerHTML = '$' + parseFloat(valSlider).toFixed(2);
+console.log(isChecked);
+
+inputCheck.addEventListener("click", (e) => {
+  isChecked = inputCheck.checked;
+  console.log(isChecked);
+});
 
 slider.oninput = function() {
-  output.innerHTML = '$' + parseFloat(this.value).toFixed(2);
-  output2.innerHTML = '$' + parseFloat(this.value).toFixed(2);
+  let newVal = this.value;
+  discount = newVal - ((25 / 100) * newVal);
+  console.log(discount);
+
+  if (isChecked === true) {
+    console.log("Check true");
+    output.innerHTML = '$' + parseFloat(discount).toFixed(2);
+    output2.innerHTML = '$' + parseFloat(discount).toFixed(2);
+  } else {
+    output.innerHTML = '$' + parseFloat(newVal).toFixed(2);
+    output2.innerHTML = '$' + parseFloat(newVal).toFixed(2);
   // outputViews.innerHTML = 
+  }
+  
 }
